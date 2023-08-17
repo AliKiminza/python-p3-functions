14484e988967cde138d806a966380cc9f468ad3d
alikiminza <ali.kiminza@student.moringaschool.com>
2023-08-17 13:55:23 +0300

    editted all functions as required

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello,{name}")
+
+greet_with_default("Sunny")    
 
 def add(num1, num2):
-    pass
+    print(num1+num2)
+
+add(num1=1,num2=2)    
 
 def halve(number):
-    pass
+    print(number/2)
+
+halve(number=4)    
